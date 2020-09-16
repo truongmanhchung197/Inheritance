55387840263069f5cae7a92722008729831fa242
Chung <truongmanhchung207@gmail.com>
2020-09-16 17:29:26 +0700

    add triangle

---
+++ Triangle/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle triangle=new Triangle(2,3,4,"red",true);
+        System.out.println(triangle);
+    }
+}
